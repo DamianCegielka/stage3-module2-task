3deb1557bb222b0539cad9e71f2975ad62c5679d
Damian Cegielka <cegielka.damian@gmail.com>
2023-01-31 11:01:27 +0100

    Add CommandFactory class

--- module-main/src/main/java/com/mjc/school/main/Menu.java
+++ module-main/src/main/java/com/mjc/school/main/Menu.java
@@ -1,11 +1,6 @@
 package com.mjc.school.main;
 
-import com.mjc.school.controller.BaseController;
-import com.mjc.school.controller.impl.command.*;
-import com.mjc.school.service.dto.AuthorDtoRequest;
-import com.mjc.school.service.dto.AuthorDtoResponse;
-import com.mjc.school.service.dto.NewsDtoRequest;
-import com.mjc.school.service.dto.NewsDtoResponse;
+import com.mjc.school.controller.impl.command.CommandFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -32,21 +27,14 @@ public class Menu {
                     0 - Exit.
                     """;
 
+
     @Autowired
-    private final BaseController<NewsDtoRequest, NewsDtoResponse, Long> newsController;
-    @Autowired
-    private final BaseController<AuthorDtoRequest, AuthorDtoResponse, Long> authorController;
-    @Autowired
-    private final Invoker invoker;
+    private final CommandFactory commandFactory;
 
     private int chosenNumber = -1;
 
-    public Menu(BaseController<NewsDtoRequest, NewsDtoResponse, Long> newsController,
-                BaseController<AuthorDtoRequest, AuthorDtoResponse, Long> authorController,
-                Invoker invoker) {
-        this.newsController = newsController;
-        this.authorController = authorController;
-        this.invoker=invoker;
+    public Menu(CommandFactory commandFactory) {
+        this.commandFactory=commandFactory;
     }
 
     public void mainController() throws IOException {
@@ -55,20 +43,7 @@ public class Menu {
             while (chosenNumber != 0) {
                 System.out.println(MENU_TEXT);
                 chosenNumber = this.takeNumberFromKeyboard();
-                switch (chosenNumber) {
-                    case 1 -> invoker.setCommand(new CreateNewsCommand(newsController));
-                    case 2 -> invoker.setCommand(new CreateAuthorCommand(authorController));
-                    case 3 -> invoker.setCommand(new ReadAllNewsCommand(newsController));
-                    case 4 -> invoker.setCommand(new ReadAllAuthorsCommand(authorController));
-                    case 5 -> invoker.setCommand(new ReadNewsByIdCommand(newsController));
-                    case 6 -> invoker.setCommand(new ReadAuthorByIdCommand(authorController));
-                    case 7 -> invoker.setCommand(new UpdateNewsCommand(newsController));
-                    case 8 -> invoker.setCommand(new UpdateAuthorCommand(authorController));
-                    case 9 -> invoker.setCommand(new DeleteNewsByIdCommand(newsController));
-                    case 10 ->invoker.setCommand(new DeleteAuthorByIdCommand(authorController));
-                    case 0 -> System.out.println("By by!");
-                    default -> System.out.println("Error!");
-                }
+                commandFactory.mainController(chosenNumber);
             }
         } catch (Exception e) {
             e.printStackTrace();
